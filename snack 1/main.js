adef57749b6864a178d61390642cf6ff9fa08892
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-11-08 17:22:33 +0100

    Modifica alla gestione degli snack in cartelle

--- snack 1/main.js
+++ snack 1/main.js
@@ -38,6 +38,8 @@ let btn = document.getElementById("btn");
 
 //  }
 
+
+
 // ?FILTER
 btn.addEventListener("click", function () {
     let min = inputMin.value;
@@ -58,17 +60,6 @@ btn.addEventListener("click", function () {
 
 
 
-// ^---------SNACK 2------
-let students = [
-    { name: 'Marco', id: 213, grades: 78 },
-    { name: 'Paola', id: 110, grades: 96 },
-    { name: 'Andrea', id: 250, grades: 48 },
-    { name: 'Gaia', id: 145, grades: 74 },
-    { name: 'Luigi', id: 196, grades: 68 },
-    { name: 'Piero', id: 102, grades: 50 },
-    { name: 'Francesca', id: 120, grades: 84 },
-];
 
-let newStudents = [];
 
 
