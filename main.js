20c1a2ff414d5c093c4bb0f23045af50da4a529a
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-11-08 17:21:25 +0100

    Snack 1 Versione Filter

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+
+// ^---------SNACK 1------
 const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
-const newArray = [];
+
 
 let inputMin = document.getElementById("inputMin");
 let inputMax = document.getElementById("inputMax");
@@ -8,34 +10,65 @@ let risultato = document.getElementById("risultato");
 let btn = document.getElementById("btn");
 
 
+// !FOR EACH
+// const newArray = [];
+// btn.addEventListener("click", function () {
+//      newArray.length = 0;
+//      let min = inputMin.value;
+//      let max = inputMax.value;
+//      stampaRisultato(min, max);
+//      console.log(newArray)
+//      risultato.innerHTML = newArray
+//  })
 
-btn.addEventListener("click", function(){
-    newArray.length=0;
+
+
+//  function stampaRisultato(min, max) {
+
+//      myArray.forEach((element, index) => {
+
+//           console.log(element)
+//           console.log(index)
+
+//          if (index >= min && index <= max) {
+
+//              newArray.push(element);
+//          }
+//      });
+
+//  }
+
+// ?FILTER
+btn.addEventListener("click", function () {
     let min = inputMin.value;
-    let max = inputMax.value; 
-    stampaRisultato(min, max);
-    console.log(newArray)
-    risultato.innerHTML = newArray
+    let max = inputMax.value;
+
+    let newArray = myArray.filter((element, index) => {
+        
+        if (index >= min && index <= max) {
+            return true;
+        }
+    })
+    
+    risultato.innerHTML = newArray;
 })
 
 
 
-function stampaRisultato(min, max){
-    
-    myArray.forEach((element, index) => {
 
-        // console.log(element)
-        // console.log(index)
 
-        if (index >= min && index <= max) {
 
-            newArray.push(element);
-        }
-    });
-    
-}
+// ^---------SNACK 2------
+let students = [
+    { name: 'Marco', id: 213, grades: 78 },
+    { name: 'Paola', id: 110, grades: 96 },
+    { name: 'Andrea', id: 250, grades: 48 },
+    { name: 'Gaia', id: 145, grades: 74 },
+    { name: 'Luigi', id: 196, grades: 68 },
+    { name: 'Piero', id: 102, grades: 50 },
+    { name: 'Francesca', id: 120, grades: 84 },
+];
 
-    
-
+let newStudents = [];
 
 
